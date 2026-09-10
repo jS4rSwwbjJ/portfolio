21e79636d44608e0f255452b7c86a0b55a81ebd9
jS4rSwwbjJ <tktkpentk.bg0330@gmail.com>
2026-09-10 22:09:45 +0900

    Update script_ka.js

--- javascript/script_ka.js
+++ javascript/script_ka.js
@@ -24,9 +24,10 @@ fetch('./javascript/modal_data_ka.json')
     canvas.height = SIZE;
     const ctx = canvas.getContext("2d");
 
-      // 一時的
-    console.log(file);
+
     gallery.appendChild(canvas);
+      // 一時的
+      console.log("canvas追加:", file);
 
     img.onload = () => {
       const fileName = file.replace(/\.[^.]+$/, "");
